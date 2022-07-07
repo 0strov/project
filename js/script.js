dd072a934dcba608e7734b059057741e590a8736
Vi <t_vik@list.ru>
2022-07-07 12:18:48 +0700

    first practice

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 'use strict';
 
-const category = 'toys';
-console.log(`https://url.com/${category}/5`);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
 
-const user = 'Ivan';
-alert(`Hey, ${user}`);
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = +prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = +prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
